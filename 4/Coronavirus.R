b1fc2da36979d87c3fce3b569e2eceaadd3ff1fd
SanteauX <welson.thumper@gmail.com>
2020-02-09 01:43:30 -0500

    fixed coronavirus propagation

--- 4/Coronavirus.R
+++ 4/Coronavirus.R
@@ -1,14 +1,19 @@
-#Coronavirus
-
-
-library(remotes)
 install_github("Guangchuangyu/nCov2019")
-Library(nCov2019)
-x = getnCov2019()
-x = load_nCov2019()
+##################https://github.com/GuangchuangYu/nCov2019############################
+library(nCov2019)
+x=get_nCov2019()
+x=load_nCov2019()
 head(x[])
-Levels(x[]$province)
+levels(x[]$province)
 x[levels(as.factor(x[]$province))[1],]
-
+#########################################################################################
 library(ggplot2)
 require(ggrepel)
+
+d=x['湖北',]
+
+ggplot(d,
+       aes(time, as.numeric(cum_confirm), group=city, color=city)) +
+  geom_point() + geom_line() +
+  theme_minimal(base_size = 14) + theme(legend.position='none') +
+  xlab(NULL) + ylab(NULL)
